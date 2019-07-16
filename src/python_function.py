a7f621378fa3514e8a67400e157fa845a17bf9eb
zang.fan <zang.fan@cnmo.com>
2019-07-16 14:01:29 +0800

    review function parameter

--- src/python_function.py
+++ src/python_function.py
@@ -2,5 +2,22 @@
 # -*- coding: utf-8 -*-
 # author：zangfans time:2019-7-10
 
+# def index(x,y,z=1):
+#     print(x,y,z)
+# def wrapper(*args,**kwargs):
+#     index(args,kwargs)
+#     index(*args, **kwargs) #就是打散了
+# wrapper(1,2,3)
+# wrapper(1,2)
+# wrapper(x=1,y=2)
+# wrapper(1,y=2)
+
+def foo(x,y,*,a,b):
+    print(x)
+    print(y)
+    print(a)
+    print(b)
+
+foo(1,2,b=20,a=10)
 
 
