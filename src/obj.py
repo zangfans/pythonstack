850e662412315fa2564d84e10e3bbaf4961f32ac
zangfans <zf.strange@gmail.com>
2019-10-29 15:55:02 +0800

    增加面向对象编程的四大支柱：封装，抽象，多态和继承 另增加一个组合

--- src/obj.py
+++ src/obj.py
@@ -2,6 +2,24 @@
 # -*- coding: utf-8 -*-
 # author：zangfans time:2019/10/24
 
+#封装：
+# 1.在面向对象编程中，对象将变量（状态）和方法（用来改变状态或执行涉及状态的计算）集中在一个地方--即对象本身。
+# 2.隐藏的内部数据，以避免客户端代码（类外部的方法）直接进行访问。
+#
+
+#抽象
+# 剥离事物的诸多特征，使其只保留最基本的特征
+
+#多态
+# 为不同的基础形态（数据类型）提供相关接口的能力。接口指的是函数或方法。
+
+#继承
+# 假设多个类存在相同的属性和行为时，我们同样可以将这些内容抽取到单独的一个类中，那么这些类就没有必要在定义这些属性和行为，只需要单独继承就可以了
+# 子类改变从父类中继承方法的继承能力，被称为方法覆盖
+
+#组合
+# 通过组合技巧，将一个对象作为变量保存在另一个对象中，可以模拟拥有关系
+
 class Orange():
     def __init__(self, w, c):
         self.weight = w
@@ -47,14 +65,27 @@ class shape():
         print('''{} by {} '''.format(self.width,self.len))
 
 
+class R1(shape):
+    pass
+
+obj2 = R1(10, 10)
+print(obj2.print_size())
 
 
+class Dog():
+    def __init__(self, name, breed, owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
 
+class Person():
+    def __init__(self, name):
+        self.name = name
 
+mick = Person("Mick jackson")
+stan = Dog("Stanley", "Bulldog", mick)
 
-
-
-
+print(stan.owner.name)
 
 
 
