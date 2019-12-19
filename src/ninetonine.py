3175b464f70675d720bbdb2bdfa3d238faac2b77
zang.fan <zang.fan@cnmo.com>
2019-12-19 16:05:54 +0800

    while循环

--- src/ninetonine.py
+++ src/ninetonine.py
@@ -21,3 +21,11 @@ else:
         else:
             print("%d 是质数!" % n)
 
+total = person = score=0
+while(person < 3):
+    person += 1
+    score = int(input("请输入第 %d 位学生的成绩：> "% person))
+    total += score
+average = total / person
+print("本班总成绩；%d 分，平均成绩：%5.2f 分" % (total, average))
+
