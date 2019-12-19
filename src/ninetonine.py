1bd129e7b9e7b6d92afaebeea9ca7d1b320138d3
zang.fan <zang.fan@cnmo.com>
2019-12-19 15:15:10 +0800

    判断质数

--- src/ninetonine.py
+++ src/ninetonine.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author：zangfans time:2019/12/19
+#for循环是有规则的，while循环是无规则的
+
 
 for i in range(1,10):
     for j in range(1,10):
@@ -8,5 +10,14 @@ for i in range(1,10):
         print("%d*%d=%-2d  " %(i, j, result), end="")
     print()
 
-
+n = int(input("请输入大于1的整数>  "))
+if (n == 2):
+    print("2 是质数！")
+else:
+    for i in (2, n):
+        if (n % i == 0):
+            print("%d 不是质数!" % n)
+            break
+        else:
+            print("%d 是质数!" % n)
 
