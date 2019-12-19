09e111967ae661b0667718a6a202a459c5878fc1
zang.fan <zang.fan@cnmo.com>
2019-12-19 18:07:39 +0800

    字典的基本用法

--- src/dic.py
+++ src/dic.py
@@ -33,19 +33,27 @@ info_dic = {'name':'zangfan','age':24,'gender':"male"}
 #     print(k,v)
 
 
+dict1 = {"apple":50, "juice":25}
+print(len(dict1))
+
+dict2 = dict1.copy()
+print(dict2)
 
 
+print(dict1.get("apple"))
+
+print("apple" in dict1)
+
+print(dict1.clear())
 
 
-
-
-
-
-
-
-
-
-
+dict2 = {"apple": 50, "juice": 25, "pear": 30}
+dict2["banana"] = 45
+dict2["watermalon"] = 65
+listkey = list(dict2.keys())
+listvalue = list(dict2.values())
+for i in range(0,len(listkey)):
+    print("%s 的价格是 %d 元" % (listkey[i], listvalue[i]))
 
 
 
